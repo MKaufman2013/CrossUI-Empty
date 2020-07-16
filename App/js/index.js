b2da27fe812b4adcd24261b14b15f3f21f69e9f3
MKaufman2013 <68393270+MKaufman2013@users.noreply.github.com>
2020-07-16 12:59:07 -0500

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,58 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.APICaller")
+                .setHost(host,"api_1")
+                .setResponseDataTarget([
+                    {
+                        "type":"alert",
+                        "name":"alert",
+                        "path":""
+                    }
+                ])
+                .setQueryURL("https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xCC4304A31d09258b0029eA7FE63d032f52e44EFe&vs_currencies=usd&include_24hr_change=true")
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button3")
+                .setDirtyMark(false)
+                .setLeft("29.714285714285715em")
+                .setTop("22.095238095238095em")
+                .setWidth("7.542857142857143em")
+                .setHeight("3.580952380952381em")
+                .setCaption("Button")
+            );
+            
+            append(
+                xui.create("xui.UI.Element")
+                .setHost(host,"xui_ui_element1")
+                .setLeft("0.7619047619047619em")
+                .setTop("1.5238095238095237em")
+                .setWidth("59.04761904761905em")
+                .setHeight("7.085714285714285em")
+                .setHtml("<pre style=\"box-sizing: inherit; font-family: &quot;Source Code Pro&quot;, monospace; margin-top: 0px; margin-bottom: 0px; overflow: auto; color: rgb(255, 255, 255); padding: 10px; white-space: pre-wrap; border-radius: 4px; background: rgb(65, 68, 78); font-weight: 600;\">https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xCC4304A31d09258b0029eA7FE63d032f52e44EFe&amp;vs_currencies=usd&amp;include_24hr_change=true</pre>")
+            );
+            
+            append(
+                xui.create("xui.MessageService")
+                .setHost(host,"xui_messageservice1")
+            );
+            
+            append(
+                xui.create("xui.MQTT")
+                .setHost(host,"xui_mqtt1")
+                .setServer("https://api.coingecko.com/api/v3/simple/token_price/ethereum?contract_addresses=0xCC4304A31d09258b0029eA7FE63d032f52e44EFe&vs_currencies=usd&include_24hr_change=true")
+            );
+            
+            append(
+                xui.create("xui.UI.CSSBox")
+                .setHost(host,"xui_ui_cssbox3")
+                .setClassName("xui-css-ky")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
